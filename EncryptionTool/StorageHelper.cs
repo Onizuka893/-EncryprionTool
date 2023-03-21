40942b3ae2c49c3f5dad4c771119ff113f287bfa
Onizuka893 <92974194+Onizuka893@users.noreply.github.com>
2023-03-21 12:07:30 +0100

    added storagehelper added keyhelper

---
+++ EncryptionTool/StorageHelper.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System.IO;
+
+namespace EncryptionTool
+{
+    public static class StorageHelper
+    {
+        public static string SetDefaultFolder()
+        {
+            var dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.Multiselect = false;
+            dialog.Title = "Select Default Folder";
+            dialog.ValidateNames = false;
+            dialog.CheckFileExists = false;
+            dialog.CheckPathExists = true;
+            dialog.FileName = "Folder Selection.";
+            if (dialog.ShowDialog() == true)
+            {
+                return Path.GetDirectoryName(dialog.FileName);
+            }
+            return null;
+        }
+
+        public static bool SaveFile(string fileContent)
+        {
+            SaveFileDialog sfd = new()
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                Title = "Save encryped file",
+                CheckPathExists = true,
+                DefaultExt = "txt",
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (sfd.ShowDialog() == true)
+            {
+                File.WriteAllText(sfd.FileName, fileContent);
+                return true;
+            }
+            return false;
+        }
+    }
+}
