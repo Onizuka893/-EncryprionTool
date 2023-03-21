cc53a23676c1d3e734b073ab49090a5a13741131
Yassine Amzil <yassine.amzil@student.pxl.be>
2023-03-21 12:24:12 +0100

    Add RSA Helper class

---
+++ EncryptionTool/RSAHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionTool
+{
+    public class RSAHelper : IEncryptor
+    {
+
+        private RSA rsaAlgorithm;
+
+        public RSAHelper()
+        {
+
+        }
+       
+        public string EncryptString(string plaintext)
+        {
+            
+        }
+
+        public string DecryptString(string plaintext)
+        {
+
+        }
+
+        public string DecryptImage(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string EncryptImage(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
