32f2a8eee101085b19916763d48536ac1054b758
Volkan Ibis <12102424@student.pxl.be>
2023-04-27 12:07:32 +0200

    asd

---
+++ EncryptionTool/FileDialogFilters.cs
@@ -0,0 +1,9 @@
+﻿namespace EncryptionTool
+{
+    public static class FileDialogFilters
+    {
+        public static readonly string[] TextFileFilter = { "Save Text File", "txt", "Text files (*.txt)|*.txt;" };
+        public static readonly string[] KeyFileFilter = { "Save Key File", "txt", "Key files (*.txt)|*.txt;" };
+        public static readonly string[] ImageFileFilter = { "Save Image File", "txt", "Text files (*.txt)|*.txt;" };
+    }
+}
